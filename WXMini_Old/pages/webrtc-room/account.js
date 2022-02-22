f0c9fb707008e430f8df2126dc0c5a657eea99eb
wesleylei <wesleylei@tencent.com>
2022-02-22 19:41:26 +0800

    老项目测试横屏

---
+++ WXMini_Old/pages/webrtc-room/account.js
@@ -0,0 +1,27 @@
+//将 TEST_ACCOUNT 的内容替换成"控制台/实时音视频/快速上手第三步"生成的配置文件内容
+const TEST_ACCOUNT = {
+  "sdkappid": 1400182283,
+  "users": [{
+    "userId": "Miniprogram_trtc_01",
+    "userToken": "eJwtzd0KgkAQBeB32VvD9jdU6AXM6iIDk0CkVRnE3XXdQorePVMv5zuHMx*UJhf-VVkUIepjtJlvkJVyUMPMR1BgrG5s2RXOukeByVobZFsaAxJFhGNMAkoDtiTVaMBWkwshKMZ4UQfd33aMhDwUTKwr0ExfONZMZfGVE51TfY7lkDCvz5pDXd8a5923-TM-pW89Kt7u0fcH2Sw3Og__"
+    },
+    {
+      "userId": "Miniprogram_trtc_02",
+      "userToken": "eJwtzV0LgjAUBuD-suuQs*l0E7qIEoI*EPKqGxG37FDamkv6oP*eqZfneV-e8yHZ9uB12pKYMA-IbLhR6cbhCQfeYYPG3ipb1LmzrsyBTbVWXQpjUJGYBgBUMCb8MdFPg1b3zjlnADCqw-pvoU9lIHkUTStY9V9Wi7Nw5XqvEiooD9OgY8avHiqrNvKavI7Lu3qjBIlpOyffH5nzNkc_"
+    },
+    {
+      "userId": "Miniprogram_trtc_03",
+      "userToken": "eJwtzcsOgjAQBdB-6dqYoaVASVyoCw3RxLeJG4JScEKApi0*Yvx3EVjOuTd3PuSw2o8fUpOQ0DGQUXdjKiuLGXa8xgqVrnOdlLHV9hYDG2omLRKlMCWh4wI4AaUB6xP5Uqhl65xzCgC9Wiz-5jFHuIILb1jBvP0CdWPYwpcRu5h5tHwHT3mE7Crszt-cm*2s8YtzFU1PJi8m5PsDr3E2-A__"
+    },
+    {
+      "userId": "Miniprogram_trtc_04",
+      "userToken": "eJwtzcEOgjAQBNB-6VWD21IQSLwQEy96MHjQxIRUi3U1haZWEI3-LgLHfTOZ-ZDdOvPqwpKEMA-ItL9RFqXDC-a8wRKNrZQVOnfWnXPgY*0h78IYlCShHIBGjEX*kBQvg7boPAgCBgCDOtR-C30a8zhk83EFVfdFimujJy09zpbvrNb*IV1helJbFVdc1NDsNTyrkNlbq-iCfH-2qzeb"
+    },
+    // {
+    //   "userId": "mini_test_05",
+    //   "userToken": "eJxlj0tPg0AYRff8CsJWI8NjQjRxQbQYG2poa1VWE9oZysdrxmEG*4j-3YpNxHi35*Te3KNhmqb1HC*vss2G61YRtRfMMm9MC1mXv1AIoCRTxJP0H2Q7AZKRLFdMDtDBGLsIjR2grFWQw9looAWiWKcIwiOroxUZpn5q-FOHFyD3jwLbAc4mq7vHqKq2ST8VXYmjna5SFKSrohBNnXDbr-PDOrlfvGv*UZWLECZhkV3Hdqhpvbb35XwmX5qp9tL5Q5y*1k8XuV4iO*p9*naI*e1oUkHDzr987DqnBCPaM9kBbwfBRQ52XA99xzI*jS8RYGEq"
+    // }
+  ]
+}
+
+module.exports = TEST_ACCOUNT;
